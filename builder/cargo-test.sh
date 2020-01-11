24e2cb83ad36c1e676b2bd75c29ae789d194bb00
x7c1 <jmtaro+x7c1@gmail.com>
2020-01-11 17:45:05 +0900

    add draft usage

--- builder/cargo-test.sh
+++ builder/cargo-test.sh
@@ -7,13 +7,22 @@ cargo build \
   --verbose \
   ${BUILD_MODE} \
   --target="${TARGET_X86}"
-
-cargo build \
-  --verbose \
-  ${BUILD_MODE} \
-  --target="${TARGET_ARM}"
+#
+#cargo build \
+#  --verbose \
+#  ${BUILD_MODE} \
+#  --target="${TARGET_ARM}"
 
 cargo test \
   ${BUILD_MODE} \
   --target="${TARGET_X86}" \
   -- --nocapture
+
+#cargo run \
+#  ${BUILD_MODE} \
+#  --target="${TARGET_X86}"
+
+"$PROJECT_ROOT"/target/x86_64-unknown-linux-musl/debug/s3api \
+  put-object \
+    --bucket=hoge \
+    --key=fuga
