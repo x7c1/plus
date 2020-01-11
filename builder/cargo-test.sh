ac77f468873fdb41790c766316143576c45c2eaf
x7c1 <jmtaro+x7c1@gmail.com>
2020-01-11 18:14:32 +0900

    enable cross compile

--- builder/cargo-test.sh
+++ builder/cargo-test.sh
@@ -7,20 +7,17 @@ cargo build \
   --verbose \
   ${BUILD_MODE} \
   --target="${TARGET_X86}"
-#
-#cargo build \
-#  --verbose \
-#  ${BUILD_MODE} \
-#  --target="${TARGET_ARM}"
+
+CC=arm-linux-gnueabihf-gcc \
+cargo build \
+  --verbose \
+  ${BUILD_MODE} \
+  --target="${TARGET_ARM}"
 
 cargo test \
   ${BUILD_MODE} \
   --target="${TARGET_X86}" \
   -- --nocapture
-
-#cargo run \
-#  ${BUILD_MODE} \
-#  --target="${TARGET_X86}"
 
 "$PROJECT_ROOT"/target/x86_64-unknown-linux-musl/debug/s3api \
   put-object \
