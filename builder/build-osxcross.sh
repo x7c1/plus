7747e4f976ae96a9c6fc43c04e81f882b0f1261c
x7c1 <jmtaro+x7c1@gmail.com>
2020-01-13 19:47:13 +0900

    skip tarball handling if sdk already installed

--- builder/build-osxcross.sh
+++ builder/build-osxcross.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if is_osx_sdk_installed; then
+  echo "osx sdk already installed."
+  return
+fi
+
 src_path="$PROJECT_ROOT/builder/$OSX_SDK"
 if [[ ! -f "$src_path" ]]; then
   echo "osx sdk tarball not found. [$src_path]"
@@ -11,11 +16,6 @@ if [[ -f "$dst_path" ]]; then
   echo "osx sdk tarball already copied. [$dst_path]"
 else
   cp ${src_path} ${dst_path}
-fi
-
-if is_osx_sdk_installed; then
-  echo "osx sdk already installed."
-  return
 fi
 
 cd "$OSXCROSS_ROOT" || exit 1
