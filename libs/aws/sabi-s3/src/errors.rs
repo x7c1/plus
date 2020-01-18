ed5cd068c95dbd037a10dbd62a63302d75f169ec
x7c1 <jmtaro+x7c1@gmail.com>
2020-01-18 12:59:52 +0900

    enrich log message

--- libs/aws/sabi-s3/src/errors.rs
+++ libs/aws/sabi-s3/src/errors.rs
@@ -4,6 +4,6 @@ pub type Result<T> = ::std::result::Result<T, Error>;
 
 #[derive(Fail, Debug)]
 pub enum Error {
-    #[fail(display = "std::io::Error")]
+    #[fail(display = "std::io::Error > {}", 0)]
     StdIoError(std::io::Error),
 }
