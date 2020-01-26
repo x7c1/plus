b70ce62a92d1df53eb7d83eb845ad0de1e31f65d
x7c1 <jmtaro+x7c1@gmail.com>
2020-01-27 02:48:54 +0900

    revert

--- libs/aws/sabi-s3/src/errors.rs
+++ libs/aws/sabi-s3/src/errors.rs
@@ -4,48 +4,21 @@ pub type Result<T> = ::std::result::Result<T, Error>;
 
 #[derive(Fail, Debug)]
 pub enum Error {
-    #[fail(display = "std::string::FromUtf8Error > {}", 0)]
-    FromUtf8Error(std::string::FromUtf8Error),
-
-    #[fail(display = "http::Error > {}", 0)]
-    HttpError(http::Error),
-
-    #[fail(display = "hyper::Error > {}", 0)]
-    HyperError(hyper::Error),
-
-    #[fail(display = "http::uri::InvalidUri > {}", 0)]
-    InvalidUri(http::uri::InvalidUri),
+    #[fail(display = "reqwest::Error > {}", 0)]
+    Reqwest(reqwest::Error),
 
     #[fail(display = "std::io::Error > {}", 0)]
     StdIoError(std::io::Error),
 }
 
-impl From<std::string::FromUtf8Error> for Error {
-    fn from(e: std::string::FromUtf8Error) -> Self {
-        Error::FromUtf8Error(e)
-    }
-}
-
-impl From<http::Error> for Error {
-    fn from(e: http::Error) -> Self {
-        Error::HttpError(e)
-    }
-}
-
-impl From<hyper::Error> for Error {
-    fn from(e: hyper::Error) -> Self {
-        Error::HyperError(e)
-    }
-}
-
-impl From<http::uri::InvalidUri> for Error {
-    fn from(e: http::uri::InvalidUri) -> Self {
-        Error::InvalidUri(e)
-    }
-}
-
 impl From<std::io::Error> for Error {
     fn from(e: std::io::Error) -> Self {
         Error::StdIoError(e)
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(e: reqwest::Error) -> Self {
+        Error::Reqwest(e)
     }
 }
