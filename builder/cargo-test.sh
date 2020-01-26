2d0cdacf7846f5fdbf87a0287dc71c6fc106e2a4
x7c1 <jmtaro+x7c1@gmail.com>
2020-01-27 00:17:46 +0900

    revert

--- builder/cargo-test.sh
+++ builder/cargo-test.sh
@@ -3,28 +3,7 @@
 . ./builder/setup-env.sh
 cd "$PROJECT_ROOT" || exit 1
 
-to_features_option() {
-  if [[ $1 = "./apps/s3api/Cargo.toml" ]]; then
-    echo "--features=sabi-s3/x86_support"
-  elif [[ $1 = "./libs/aws/sabi-s3/Cargo.toml" ]]; then
-    echo "--features=x86_support"
-  else
-    echo ""
-  fi
-}
-
-get_manifests() {
-  find ./ -type f -name "Cargo.toml" \
-    | grep -v "\./Cargo.toml" \
-    | sort
-}
-
-for manifest in $(get_manifests) ; do
-  echo ">> $manifest"
-  cargo test \
-    --manifest-path=$manifest \
-    $(to_features_option $manifest) \
-    ${BUILD_MODE} \
-    --target="${TARGET_X86}" \
-    -- --nocapture
-done
+cargo test \
+  ${BUILD_MODE} \
+  --target="${TARGET_X86}" \
+  -- --nocapture
