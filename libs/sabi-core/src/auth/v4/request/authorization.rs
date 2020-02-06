59b7378fba7345b4e10c1f8863c4eb6b85b8b74b
x7c1 <jmtaro+x7c1@gmail.com>
2020-02-07 01:17:20 +0900

    remove needless struct

--- libs/sabi-core/src/auth/v4/request/authorization.rs
+++ libs/sabi-core/src/auth/v4/request/authorization.rs
@@ -1,21 +1,23 @@
+use crate::auth::account::AccessKey;
 use crate::auth::v4::calculator::Signature;
 use crate::auth::v4::canonical::SignedHeaders;
-use crate::auth::v4::request::Credential;
-use crate::auth::v4::sign::Algorithm;
+use crate::auth::v4::sign::{Algorithm, CredentialScope};
 
 pub struct Authorization(String);
 
 impl Authorization {
     pub fn new(
         algorithm: &Algorithm,
-        credential: &Credential,
+        access_key: &AccessKey,
+        scope: &CredentialScope,
         signed_header: &SignedHeaders,
         signature: &Signature,
     ) -> Authorization {
         Authorization(format!(
-            "{} Credential={},SignedHeaders={},Signature={}",
+            "{} Credential={}/{},SignedHeaders={},Signature={}",
             algorithm.as_str(),
-            credential.as_str(),
+            access_key.as_str(),
+            scope.as_str(),
             signed_header.as_str(),
             signature.as_str(),
         ))
--- libs/sabi-core/src/auth/v4/request/mod.rs
+++ libs/sabi-core/src/auth/v4/request/mod.rs
@@ -1,5 +1,2 @@
 mod authorization;
 pub use authorization::Authorization;
-
-mod credential;
-pub use credential::Credential;
