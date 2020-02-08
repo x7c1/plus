7f83a5ee557bf76e7b660fe9061b48fc0609fe69
x7c1 <jmtaro+x7c1@gmail.com>
2020-02-08 17:10:07 +0900

    add ContentType

---
+++ libs/sabi-core/src/http/header/content_type.rs
@@ -0,0 +1,12 @@
+#[derive(Debug)]
+pub struct ContentType(String);
+
+impl ContentType {
+    pub fn new<A: Into<String>>(key: A) -> Self {
+        Self(key.into())
+    }
+
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
--- libs/sabi-core/src/http/header/mod.rs
+++ libs/sabi-core/src/http/header/mod.rs
@@ -3,3 +3,6 @@ pub use header_fragment::HeaderFragment;
 
 mod headers;
 pub use headers::Headers;
+
+mod content_type;
+pub use content_type::ContentType;
