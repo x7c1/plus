c0f894dc6b5b44f72f2ad7bb07c276dbf770d4be
x7c1 <jmtaro+x7c1@gmail.com>
2020-02-10 23:28:09 +0900

    mask SecretKey

--- libs/sabi-core/src/auth/account/secret_key.rs
+++ libs/sabi-core/src/auth/account/secret_key.rs
@@ -1,6 +1,6 @@
 use crate::verbs::AsBytes;
+use std::fmt;
 
-#[derive(Debug)]
 pub struct SecretKey(String);
 
 impl SecretKey {
@@ -16,5 +16,30 @@ impl SecretKey {
 impl AsBytes for SecretKey {
     fn as_bytes(&self) -> &[u8] {
         self.0.as_bytes()
+    }
+}
+
+impl fmt::Debug for SecretKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let visible_length = 4;
+        let masked = {
+            let start = self.0.len() - visible_length;
+            format!("{:*>20}", &self.0[start..])
+        };
+        f.debug_tuple("SecretKey").field(&masked).finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SecretKey;
+
+    #[test]
+    fn it_should_be_masked() {
+        let key = SecretKey::new("wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY");
+        let masked = format!("{:?}", key);
+
+        assert_eq!(masked, r#"SecretKey("****************EKEY")"#);
+        assert_eq!(masked.len(), 20 + r#"SecretKey("")"#.len());
     }
 }
