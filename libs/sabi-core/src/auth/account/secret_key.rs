c452fa9835ca4bd3306b9b00914a17eb9f00c9d2
x7c1 <jmtaro+x7c1@gmail.com>
2020-02-11 17:20:25 +0900

    fix bug which panics multibyte chars

--- libs/sabi-core/src/auth/account/secret_key.rs
+++ libs/sabi-core/src/auth/account/secret_key.rs
@@ -1,4 +1,5 @@
 use crate::verbs::AsBytes;
+use characters::{AsStr, MultiByte};
 use std::fmt;
 
 pub struct SecretKey(String);
@@ -6,10 +7,6 @@ pub struct SecretKey(String);
 impl SecretKey {
     pub fn new<A: Into<String>>(value: A) -> Self {
         Self(value.into())
-    }
-
-    pub fn as_str(&self) -> &str {
-        self.0.as_str()
     }
 }
 
@@ -19,13 +16,17 @@ impl AsBytes for SecretKey {
     }
 }
 
+impl AsStr for SecretKey {
+    fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+impl MultiByte for SecretKey {}
+
 impl fmt::Debug for SecretKey {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        let visible_length = 4;
-        let masked = {
-            let start = self.0.len() - visible_length;
-            format!("{:*>20}", &self.0[start..])
-        };
+        let masked = format!("{:*>20}", &self.tail(4));
         f.debug_tuple("SecretKey").field(&masked).finish()
     }
 }
@@ -41,5 +42,14 @@ mod tests {
 
         assert_eq!(masked, r#"SecretKey("****************EKEY")"#);
         assert_eq!(masked.len(), 20 + r#"SecretKey("")"#.len());
+    }
+
+    #[test]
+    fn it_should_be_masked_even_if_multibyte_given() {
+        let key = SecretKey("wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAM途中に日本語".to_string());
+        let masked = format!("{:?}", key);
+
+        assert_eq!(masked, r#"SecretKey("****************に日本語")"#);
+        assert_eq!(masked.chars().count(), 20 + r#"SecretKey("")"#.len());
     }
 }
