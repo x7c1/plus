e0adca577aa8c3ebb254e83404b5d0155fe095d1
x7c1 <jmtaro+x7c1@gmail.com>
2020-02-16 23:36:47 +0900

    enable to specify --pilot-output path

--- builder/build-pilot.sh
+++ builder/build-pilot.sh
@@ -7,12 +7,32 @@ cd "$PROJECT_ROOT" || exit 1
 
 set -x
 
-cargo_fmt
+main() {
+  cargo_fmt
 
-build_apps_for_linux_x86
+  build_apps_for_linux_x86
 
-run_unit_tests_for_linux_x86
+  run_unit_tests_for_linux_x86
 
-build_e2e_tests_for_linux_x86 \
-  | sed s/^\\${PROJECT_ROOT}/\\./ \
-  > ${PROJECT_ROOT}/scripts/run_pilot_path.tmp
+  build_e2e_tests_for_linux_x86 \
+    | sed s/^\\${PROJECT_ROOT}/\\./ \
+    > ${PROJECT_ROOT}/scripts/$(get_pilot_output_path $@)
+}
+
+get_pilot_output_path() {
+  while getopts ":-:" OPT; do
+    case "${OPT}" in
+      -)  case "${OPTARG}" in
+            pilot-output=*)
+              echo ${OPTARG#*=}
+              exit
+              ;;
+          esac
+          ;;
+    esac
+  done
+  echo "--pilot-output required."
+  exit 1
+}
+
+main $@
