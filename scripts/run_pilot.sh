090263765b8930aae108673396b74f4445de1235
x7c1 <jmtaro+x7c1@gmail.com>
2020-02-16 23:07:20 +0900

    add run_pilot.sh

---
+++ scripts/run_pilot.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+# stop if non-zero returned.
+set -e
+
+# not allow undefined values.
+set -u
+
+# show executed commands.
+set -x
+
+output=run_pilot_path.tmp
+
+./scripts/run_builder.sh \
+  build-pilot.sh \
+    --debug \
+    --opt-level=0 \
+    --pilot-output=${output}
+
+$(cat ./scripts/${output})
