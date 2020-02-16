492f2ba69cee3494e7af787929718908a74b48f7
x7c1 <jmtaro+x7c1@gmail.com>
2020-02-16 19:14:28 +0900

    remove needless lines

--- builder/copy-as-artifacts.sh
+++ builder/copy-as-artifacts.sh
@@ -3,9 +3,6 @@
 . ./builder/setup-env.sh
 cd "$PROJECT_ROOT" || exit 1
 
-# show executed commands.
-set -x
-
 for arch in $(get_arch_labels); do
   . ./builder/lib.${arch}.sh
   copy_release_apps_for_${arch}
