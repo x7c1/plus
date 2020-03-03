c3bdf9c869a0cf0fe52f91c6b57f0c42bff41f82
x7c1 <jmtaro+x7c1@gmail.com>
2020-03-04 01:46:03 +0900

    remove unnecessary binding

--- libs/sabi-core/src/io/stream/bytes_stream.rs
+++ libs/sabi-core/src/io/stream/bytes_stream.rs
@@ -7,6 +7,5 @@ use tokio_util::codec::{BytesCodec, FramedRead};
 /// rf. [Turning a file into futures Stream](https://users.rust-lang.org/t/turning-a-file-into-futures-stream/33480/2)
 pub fn from_file(file: File) -> impl Stream<Item = io::Result<Bytes>> {
     let decoder = BytesCodec::new();
-    let stream = FramedRead::new(file, decoder).map_ok(BytesMut::freeze);
-    stream
+    FramedRead::new(file, decoder).map_ok(BytesMut::freeze)
 }
