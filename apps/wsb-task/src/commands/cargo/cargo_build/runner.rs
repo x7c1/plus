e9aea8ebc5a0e260230613d08b9893a140c6f0f6
x7c1 <jmtaro+x7c1@gmail.com>
2020-03-14 17:24:51 +0900

    make cohesion higher

--- apps/wsb-task/src/commands/cargo/cargo_build/runner.rs
+++ apps/wsb-task/src/commands/cargo/cargo_build/runner.rs
@@ -19,40 +19,52 @@ where
         .env("RUSTFLAGS", "-C opt-level=0")
 }
 
-impl CommandProvider for LinuxX86 {
-    type Params = cargo_build::Params<Self>;
-    type Err = crate::Error;
+mod linux_x86 {
+    use super::*;
 
-    fn define(&self, params: &Self::Params) -> TaskResult<Runner<Unprepared>> {
-        let runner = base_runner(params);
-        Ok(runner)
+    impl CommandProvider for LinuxX86 {
+        type Params = cargo_build::Params<Self>;
+        type Err = crate::Error;
+
+        fn define(&self, params: &Self::Params) -> TaskResult<Runner<Unprepared>> {
+            let runner = base_runner(params);
+            Ok(runner)
+        }
     }
+    impl ShouldRun for LinuxX86 {}
 }
-impl ShouldRun for LinuxX86 {}
 
-impl CommandProvider for LinuxArmV7 {
-    type Params = cargo_build::Params<Self>;
-    type Err = crate::Error;
+mod linux_arm_v7 {
+    use super::*;
 
-    fn define(&self, params: &Self::Params) -> TaskResult<Runner<Unprepared>> {
-        let runner = base_runner(params).env("CC", "arm-linux-gnueabihf-gcc");
-        Ok(runner)
+    impl CommandProvider for LinuxArmV7 {
+        type Params = cargo_build::Params<Self>;
+        type Err = crate::Error;
+
+        fn define(&self, params: &Self::Params) -> TaskResult<Runner<Unprepared>> {
+            let runner = base_runner(params).env("CC", "arm-linux-gnueabihf-gcc");
+            Ok(runner)
+        }
     }
+    impl ShouldRun for LinuxArmV7 {}
 }
-impl ShouldRun for LinuxArmV7 {}
 
-impl CommandProvider for MacX86 {
-    type Params = cargo_build::Params<Self>;
-    type Err = crate::Error;
+mod mac_x86 {
+    use super::*;
 
-    fn define(&self, params: &Self::Params) -> TaskResult<Runner<Unprepared>> {
-        let runner = base_runner(params).env("CC", "x86_64-apple-darwin19-clang");
-        Ok(runner)
+    impl CommandProvider for MacX86 {
+        type Params = cargo_build::Params<Self>;
+        type Err = crate::Error;
+
+        fn define(&self, params: &Self::Params) -> TaskResult<Runner<Unprepared>> {
+            let runner = base_runner(params).env("CC", "x86_64-apple-darwin19-clang");
+            Ok(runner)
+        }
     }
-}
-impl MayRun for MacX86 {
-    fn unsupported(&self) -> Option<UnsupportedReport> {
-        // todo: check if sdk exists
-        None
+    impl MayRun for MacX86 {
+        fn unsupported(&self) -> Option<UnsupportedReport> {
+            // todo: check if sdk exists
+            None
+        }
     }
 }
