05f1fec8802634e438eea8efdfb310c99a1b56a5
x7c1 <jmtaro+x7c1@gmail.com>
2020-03-17 20:41:06 +0900

    avoid needless build

--- apps/wsb-task/src/commands/cargo/cargo_build/runner.rs
+++ apps/wsb-task/src/commands/cargo/cargo_build/runner.rs
@@ -12,8 +12,9 @@ where
     // todo: move opt-level to params
     command::program("cargo")
         .arg("build")
-        .arg("--verbose")
+        // .arg("--verbose")
         .args(&["--target", &params.target.as_triple()])
+        .args(&["--workspace", "--exclude=shellwork", "--exclude=wsb-task"])
         .env("RUSTFLAGS", "-C opt-level=0")
 }
 
