c7da956550e5991aaf45674ef34da13bb4584e28
x7c1 <jmtaro+x7c1@gmail.com>
2020-03-24 01:47:17 +0900

    draft

--- apps/wsb-task/src/tasks/assemble_pilot_tests.rs
+++ apps/wsb-task/src/tasks/assemble_pilot_tests.rs
@@ -1,10 +1,11 @@
-use crate::commands::build_pilot::OutputKind;
+use crate::commands::build_pilot::{OutputKind, Params};
+use crate::commands::support::Definable;
 use crate::commands::{build_pilot, copy_as_artifact, Action};
-use crate::core::targets::BuildTarget;
+use crate::core::targets::{BuildTarget, LinuxArmV7, LinuxX86, MacX86};
 use crate::{TaskOutput, TaskResult};
 use clap::{App, ArgMatches, SubCommand};
 use clap_task::ClapTask;
-use shellwork::core::command::{Runnable, RunnerOutput};
+use shellwork::core::command::{CanDefine, Runnable, RunnerOutput};
 use std::path::Path;
 
 pub fn define() -> Box<dyn ClapTask<TaskResult<TaskOutput>>> {
@@ -24,11 +25,18 @@ impl ClapTask<TaskResult<TaskOutput>> for Task {
     }
 
     async fn run<'a>(&'a self, matches: &'a ArgMatches<'a>) -> TaskResult<TaskOutput> {
+        // let s = Hoge0::build(LinuxX86, matches);
+
         try_foreach_targets!(|target| {
+            // Hoge0::build(target.clone(), matches);
+
             // build and show compile error if exists.
             let to_params = to_params_for(OutputKind::Default);
             let (action, params) = Action::from(target.clone(), matches, to_params);
             action.spawn(&params)?;
+
+            // Foooooo::build(target.clone(), matches)?;
+            // let output = Foooooo::build2(target.clone(), matches)?;
 
             // build and get filename of pilot-test.
             let to_params = to_params_for(OutputKind::FileName);
@@ -43,6 +51,38 @@ impl ClapTask<TaskResult<TaskOutput>> for Task {
             TaskResult::Ok(())
         });
         Ok(TaskOutput::empty())
+    }
+}
+
+impl Foooooo for LinuxX86 {}
+impl Foooooo for LinuxArmV7 {}
+impl Foooooo for MacX86 {}
+
+trait Foooooo
+where
+    Self: BuildTarget,
+    Self: Clone,
+    Action<Self, build_pilot::Params<Self>>: Runnable,
+    <Action<Self, build_pilot::Params<Self>> as CanDefine>::Err: From<shellwork::Error>,
+    <Action<Self, build_pilot::Params<Self>> as CanDefine>::Params: From<build_pilot::Params<Self>>,
+    crate::Error: From<<Action<Self, build_pilot::Params<Self>> as CanDefine>::Err>,
+{
+    /// build and show compile error if exists.
+    fn build(target: Self, matches: &ArgMatches) -> TaskResult<()> {
+        let params = create_params(target.clone(), matches, OutputKind::Default);
+        let action = Action::create(&target, &params);
+        action.spawn(&params.into())?;
+        Ok(())
+    }
+
+    fn build2(target: Self, matches: &ArgMatches) -> TaskResult<Option<RunnerOutput>> {
+        // build and get filename of pilot-test.
+        // let to_params = to_params_for(OutputKind::FileName);
+        // let (action, params) = Action::from(target.clone(), matches, to_params);
+        let params = create_params(target.clone(), matches, OutputKind::FileName);
+        let action = Action::create(&target, &params);
+        let output = action.capture(&params.into())?;
+        Ok(output)
     }
 }
 
