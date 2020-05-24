2b723e59ff71a5ec87b6cd9301b5d57d1c8f4dfa
x7c1 <jmtaro+x7c1@gmail.com>
2020-05-24 15:05:33 +0900

    separate runners from commands

--- apps/wsb-task/src/tasks/assemble_pilot_tests/task/mod.rs
+++ apps/wsb-task/src/tasks/assemble_pilot_tests/task/mod.rs
@@ -5,51 +5,88 @@ use crate::commands::copy_as_artifact;
 use crate::core::targets::{AsBuildTarget, BuildTarget};
 use crate::core::ActionOutput;
 use crate::TaskResult;
-use shellwork::core::command::no_op;
+use shellwork::core::command::{no_op, Runner, Unprepared};
 use std::path::Path;
 
 pub struct Task;
 
 impl Task {
     pub fn start<P: AsBuildTarget>(&self, params: &P) -> TaskResult<()> {
-        let commands = TaskCommands {
-            target: *params.as_build_target(),
-        };
+        let commands = TaskCommands::new(params);
         // todo: ignore unsupported target like macOS
         commands.run()
     }
 }
 
 struct TaskCommands {
-    target: BuildTarget,
+    runners: CommandRunners,
 }
 
 impl TaskCommands {
-    fn run(&self) -> TaskResult<()> {
+    pub fn new<P: AsBuildTarget>(params: &P) -> TaskCommands {
+        TaskCommands {
+            runners: CommandRunners {
+                target: *params.as_build_target(),
+            },
+        }
+    }
+
+    pub fn run(&self) -> TaskResult<()> {
         self.build_pilot()?;
         let output = self.get_pilot_file_name()?;
         self.copy_pilot_file(output)
     }
 
     fn build_pilot(&self) -> TaskResult<()> {
-        let params = self.params_to_build_pilot(OutputKind::Default);
-        let runner = build_pilot::create_runner(&params);
-        runner.prepare(no_op::<crate::Error>)?.spawn()?;
+        self.runners
+            .to_build_pilot()
+            .prepare(no_op::<crate::Error>)?
+            .spawn()?;
+
         Ok(())
     }
 
     fn get_pilot_file_name(&self) -> TaskResult<ActionOutput<build_pilot::Params>> {
-        let params = self.params_to_build_pilot(OutputKind::FileName);
-        let runner = build_pilot::create_runner(&params);
-        let output = runner.prepare(no_op::<crate::Error>)?.capture()?;
+        let output = self
+            .runners
+            .to_get_pilot_file_name()
+            .prepare(no_op::<crate::Error>)?
+            .capture()?;
+
         Ok(ActionOutput::new(output))
     }
 
     fn copy_pilot_file(&self, output: ActionOutput<build_pilot::Params>) -> TaskResult<()> {
-        let params = self.params_to_copy_pilot(output);
-        let runner = copy_as_artifact::create_runner(&params);
-        runner.prepare(no_op::<crate::Error>)?.spawn()?;
+        self.runners
+            .to_copy_pilot_file(output)
+            .prepare(no_op::<crate::Error>)?
+            .spawn()?;
+
         Ok(())
+    }
+}
+
+struct CommandRunners {
+    target: BuildTarget,
+}
+
+impl CommandRunners {
+    pub fn to_build_pilot(&self) -> Runner<Unprepared> {
+        let params = self.params_to_build_pilot(OutputKind::Default);
+        build_pilot::create_runner(&params)
+    }
+
+    pub fn to_get_pilot_file_name(&self) -> Runner<Unprepared> {
+        let params = self.params_to_build_pilot(OutputKind::FileName);
+        build_pilot::create_runner(&params)
+    }
+
+    pub fn to_copy_pilot_file(
+        &self,
+        output: ActionOutput<build_pilot::Params>,
+    ) -> Runner<Unprepared> {
+        let params = self.params_to_copy_pilot(output);
+        copy_as_artifact::create_runner(&params)
     }
 
     fn params_to_build_pilot(&self, kind: OutputKind) -> build_pilot::Params {
