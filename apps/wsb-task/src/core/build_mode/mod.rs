e71511476eff50b94fc728233f140b675b0f3d08
x7c1 <jmtaro+x7c1@gmail.com>
2020-06-15 00:43:07 +0900

    add core::build_mode

---
+++ apps/wsb-task/src/core/build_mode/mod.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+pub enum BuildMode {
+    Debug,
+    Release,
+}
+
+impl BuildMode {
+    pub fn all() -> Vec<BuildMode> {
+        vec![BuildMode::Debug, BuildMode::Release]
+    }
+}
+
+pub trait AsBuildMode {
+    fn as_build_mode(&self) -> &BuildMode;
+}
--- apps/wsb-task/src/core/mod.rs
+++ apps/wsb-task/src/core/mod.rs
@@ -1,6 +1,8 @@
 mod action;
 pub use action::ActionOutput;
 
+pub mod build_mode;
+
 pub mod env;
 
 pub mod support;
