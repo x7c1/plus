5faec24aa61eb79f44de6efa1e3a43af3ccf547a
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-17 01:07:32 +0900

    update workflow settings

--- builder/init.linux_x86.sh
+++ builder/init.linux_x86.sh
@@ -2,6 +2,9 @@
 
 set -xue
 
+# todo: share this value with init.sh and Dockerfile
+PLUS_RUST_VERSION="1.42.0"
+
 main() {
   rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-gnu
   rustup target add --toolchain "${PLUS_RUST_VERSION}" x86_64-unknown-linux-musl
