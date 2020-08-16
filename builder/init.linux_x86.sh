8095165383022483aef72aab3eaf5d862124383a
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-17 01:38:40 +0900

    update workflow settings

--- builder/init.linux_x86.sh
+++ builder/init.linux_x86.sh
@@ -7,8 +7,9 @@ PLUS_RUST_VERSION="1.42.0"
 
 main() {
 #  rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-gnu
-  rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-musl
+#  rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-musl
   rustup target add --toolchain "${PLUS_RUST_VERSION}" x86_64-unknown-linux-musl
+  rustup override set ${PLUS_RUST_VERSION}
   echo "done!"
 }
 
