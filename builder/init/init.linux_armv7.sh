6071db8b3fac4004f5ca478c7046a796426d0e51
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-18 00:33:23 +0900

    update workflow settings

--- builder/init/init.linux_armv7.sh
+++ builder/init/init.linux_armv7.sh
@@ -3,7 +3,7 @@
 set -xue
 
 main() {
-  rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-gnu
+#  rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-gnu
   rustup target add --toolchain ${PLUS_RUST_VERSION} armv7-unknown-linux-musleabihf
   echo "done!"
 }
