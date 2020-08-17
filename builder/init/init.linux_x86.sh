728cedae9b75a09b6af5d7f967a775860553ae82
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-18 00:31:19 +0900

    update workflow settings

--- builder/init/init.linux_x86.sh
+++ builder/init/init.linux_x86.sh
@@ -7,7 +7,7 @@ set -xue
 . ./builder/init/env.sh
 
 main() {
-#  rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-gnu
+  rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-gnu
 #  rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-musl
   rustup target add --toolchain "${PLUS_RUST_VERSION}" x86_64-unknown-linux-musl
   echo "done!"
