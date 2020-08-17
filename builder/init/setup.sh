9ca0943cdd864f7cc3842744d63119155c004d60
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-18 02:12:18 +0900

    update workflow settings

--- builder/init/setup.sh
+++ builder/init/setup.sh
@@ -5,5 +5,14 @@ set -xue
 PLUS_RUST_VERSION="1.42.0"
 export PLUS_RUST_VERSION
 
+if command -v rustc; then
+  echo "rust already installed."
+else
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /usr/bin/rustup-init
+  chmod +x /usr/bin/rustup-init
+  rustup-init -y
+  source $HOME/.cargo/env
+fi
+
 rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-gnu
 rustup override set "${PLUS_RUST_VERSION}"
