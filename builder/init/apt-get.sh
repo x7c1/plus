e53b03da596fdcf154a8962f2213e75cdf113b02
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-18 03:27:00 +0900

    enable to skip apt-get if unnecessary

--- builder/init/apt-get.sh
+++ builder/init/apt-get.sh
@@ -2,9 +2,30 @@
 
 set -xue
 
+file=$0
+hash=$(sha1sum "$file")
+hash_marker="$file".sha1
+
 main() {
+  if already_latest; then
+    echo "latest modules already installed."
+    exit
+  fi
   setup_apt
   echo "done!"
+  sha1sum "$file" > "$file".sha1
+}
+
+already_latest() {
+  if [ ! -e "$hash_marker" ]; then
+    return 1
+  fi
+  current=$(cat "$hash_marker")
+  if [ "$current" = "$hash" ]; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 setup_apt() {
