845a6106b02f8d4dbda2047d8a6f5c661726296e
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-18 01:09:45 +0900

    update workflow settings

---
+++ builder/init/tools.linux_x86.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -xue
+
+rustup component add rustfmt --toolchain "$PLUS_RUST_VERSION"-x86_64-unknown-linux-gnu
+rustup component add clippy --toolchain "$PLUS_RUST_VERSION"-x86_64-unknown-linux-gnu
+
+exit 1
