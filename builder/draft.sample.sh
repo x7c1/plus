29441b3240675a6fedc1193fc350c0cd77711ef0
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-18 23:04:13 +0900

    update draft template

--- builder/draft.sample.sh
+++ builder/draft.sample.sh
@@ -13,8 +13,28 @@ cd "$PLUS_PROJECT_ROOT" || exit 1
 
 main() {
   set -x
-
+  setup_rust
   for_task_runner
+}
+
+setup_rust() {
+  if [ -e "$HOME"/.cargo/env ]; then
+    # shellcheck source=/dev/null
+    . "$HOME"/.cargo/env
+  fi
+
+  if command -v rustc; then
+    echo "rust already installed."
+    return
+  fi
+
+  apt-get install -y curl
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /usr/bin/rustup-init
+  chmod +x /usr/bin/rustup-init
+  rustup-init -y
+
+  # shellcheck source=/dev/null
+  . "$HOME"/.cargo/env
 }
 
 for_task_runner() {
--- builder/init/setup.sh
+++ builder/init/setup.sh
@@ -5,14 +5,5 @@ set -xue
 PLUS_RUST_VERSION="1.42.0"
 export PLUS_RUST_VERSION
 
-#if command -v rustc; then
-#  echo "rust already installed."
-#else
-#  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /usr/bin/rustup-init
-#  chmod +x /usr/bin/rustup-init
-#  rustup-init -y
-#  . $HOME/.cargo/env
-#fi
-
 rustup toolchain install "${PLUS_RUST_VERSION}"-x86_64-unknown-linux-gnu
 rustup override set "${PLUS_RUST_VERSION}"
