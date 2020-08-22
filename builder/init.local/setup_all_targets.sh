c78acc07ecb4313544d8097b2bf48729e1e27459
x7c1 <jmtaro+x7c1@gmail.com>
2020-08-23 00:55:58 +0900

    separate run-if-changed.sh from app-get.sh

--- builder/init.local/setup_all_targets.sh
+++ builder/init.local/setup_all_targets.sh
@@ -3,11 +3,21 @@
 set -xue
 
 export DEBIAN_FRONTEND="noninteractive"
-./builder/init/apt-get.sh
 
-. ./builder/init.local/setup_rust.sh
-. ./builder/init/setup.sh
+main() {
+  run ./builder/init/apt-get.sh
 
-./builder/init/init.linux_x86.sh
-./builder/init/tools.linux_x86.sh
-./builder/init/init.linux_armv7.sh
+  . ./builder/init.local/setup_rust.sh
+  . ./builder/init/env.sh
+
+  run ./builder/init/init.sh
+  run ./builder/init/init.linux_x86.sh
+  run ./builder/init/tools.linux_x86.sh
+  run ./builder/init/init.linux_armv7.sh
+}
+
+run() {
+  ./builder/init/run-if-changed.sh "$1"
+}
+
+main
