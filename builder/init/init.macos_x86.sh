68da5c2ecf596df98dce4210ca032c83637f4afe
x7c1 <jmtaro+x7c1@gmail.com>
2020-09-14 00:14:29 +0900

    remove needless codes

--- builder/init/init.macos_x86.sh
+++ builder/init/init.macos_x86.sh
@@ -2,37 +2,39 @@
 
 set -xue
 
-main () {
-  rustup target add --toolchain "${PLUS_RUST_VERSION}" x86_64-apple-darwin
+rustup target add --toolchain "${PLUS_RUST_VERSION}" x86_64-apple-darwin
 
-  if is_osxcross_installed; then
-    printf "[skip] already installed: osxcross\n\n"
-  else
-    extract_osxcross
-  fi
-}
+#main () {
+#  rustup target add --toolchain "${PLUS_RUST_VERSION}" x86_64-apple-darwin
 
-extract_osxcross() {
-  cd "${PLUS_PROJECT_ROOT}/builder"
-  tar -xvf osxcross.tar.xz
-}
+#  if is_osxcross_installed; then
+#    printf "[skip] already installed: osxcross\n\n"
+#  else
+#    extract_osxcross
+#  fi
+#}
 
-setup_osxcross() {
-  if [ ! -d "$OSXCROSS_ROOT" ]; then
-    printf "[skip] not found: osxcross\n\n"
-    return
-  fi
-  cd "$OSXCROSS_ROOT" ||  exit 1
-  UNATTENDED=1 ./build.sh
-}
+#extract_osxcross() {
+#  cd "${PLUS_PROJECT_ROOT}/builder"
+#  tar -xvf osxcross.tar.xz
+#}
+#
+#setup_osxcross() {
+#  if [ ! -d "$OSXCROSS_ROOT" ]; then
+#    printf "[skip] not found: osxcross\n\n"
+#    return
+#  fi
+#  cd "$OSXCROSS_ROOT" ||  exit 1
+#  UNATTENDED=1 ./build.sh
+#}
+#
+#is_osxcross_installed() {
+#  target=${OSXCROSS_ROOT}/target/bin/${OSX_SDK_CC}
+#  if [ -f "${target}" ]; then
+#    return 0
+#  else
+#    return 1
+#  fi
+#}
 
-is_osxcross_installed() {
-  target=${OSXCROSS_ROOT}/target/bin/${OSX_SDK_CC}
-  if [ -f "${target}" ]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-main
+#main
