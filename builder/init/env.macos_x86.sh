7c40112a797ecf651d9a7c98d00b8d5e94560560
x7c1 <jmtaro+x7c1@gmail.com>
2020-09-13 20:52:34 +0900

    add env.macos_x86.sh

---
+++ builder/init/env.macos_x86.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ue
+
+export OSX_SDK_CC="x86_64-apple-darwin19-clang"
+export OSXCROSS_ROOT="${PLUS_PROJECT_ROOT}/osxcross"
+export PATH=${OSXCROSS_ROOT}/target/bin:$PATH
