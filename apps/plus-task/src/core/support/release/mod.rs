5c1a2b1349a925f696f57e3081d99c5f25c75af6
x7c1 <jmtaro+x7c1@gmail.com>
2020-09-27 17:33:50 +0900

    remove duplicates

--- apps/plus-task/src/core/support/release/mod.rs
+++ apps/plus-task/src/core/support/release/mod.rs
@@ -9,3 +9,27 @@ pub use changed_files::ChangedFiles;
 
 mod packages;
 pub use packages::PackageName;
+
+use crate::TaskResult;
+
+pub fn start(cargo_toml: &CargoToml) -> TaskResult<()> {
+    let terminal = ReleaseTerminal::load(&cargo_toml)?;
+    if cargo_toml.settings.on_crates_io {
+        terminal.cargo_publish()?;
+    }
+    if cargo_toml.settings.has_git_tag {
+        terminal.git_config()?;
+        terminal.git_tag()?;
+        terminal.git_push()?;
+    }
+    Ok(())
+}
+
+pub fn start_dry_run(cargo_toml: &CargoToml) -> TaskResult<()> {
+    let terminal = ReleaseTerminal::load(&cargo_toml)?;
+    if cargo_toml.settings.on_crates_io {
+        terminal.cargo_search()?;
+        terminal.cargo_publish_dry_run()?;
+    }
+    Ok(())
+}
--- apps/plus-task/src/tasks/release_apps/task.rs
+++ apps/plus-task/src/tasks/release_apps/task.rs
@@ -1,4 +1,5 @@
-use crate::core::support::release::{CargoToml, ChangedFiles, PackageName, ReleaseTerminal};
+use crate::core::support::release;
+use crate::core::support::release::{ChangedFiles, PackageName, ReleaseTerminal};
 use crate::TaskResult;
 
 pub struct Task;
@@ -9,7 +10,7 @@ impl Task {
         params
             .files
             .filter_cargo_tomls(&params.target_packages)
-            .try_for_each(|toml| self.start(&toml?))
+            .try_for_each(|toml| release::start(&toml?))
     }
 
     pub fn release_dry_run(&self, params: &Params) -> TaskResult<()> {
@@ -17,29 +18,7 @@ impl Task {
         params
             .files
             .filter_cargo_tomls(&params.target_packages)
-            .try_for_each(|toml| self.start_dry_run(&toml?))
-    }
-
-    fn start(&self, cargo_toml: &CargoToml) -> TaskResult<()> {
-        let terminal = ReleaseTerminal::load(&cargo_toml)?;
-        if cargo_toml.settings.on_crates_io {
-            terminal.cargo_publish()?;
-        }
-        if cargo_toml.settings.has_git_tag {
-            terminal.git_config()?;
-            terminal.git_tag()?;
-            terminal.git_push()?;
-        }
-        Ok(())
-    }
-
-    fn start_dry_run(&self, cargo_toml: &CargoToml) -> TaskResult<()> {
-        let terminal = ReleaseTerminal::load(&cargo_toml)?;
-        if cargo_toml.settings.on_crates_io {
-            terminal.cargo_search()?;
-            terminal.cargo_publish_dry_run()?;
-        }
-        Ok(())
+            .try_for_each(|toml| release::start_dry_run(&toml?))
     }
 }
 
