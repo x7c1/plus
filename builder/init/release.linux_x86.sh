d44767cde647de8a5e6840ff63c9d9c9188b706a
x7c1 <jmtaro+x7c1@gmail.com>
2020-11-09 01:48:04 +0900

    run release.linux_x86.sh

--- builder/init/release.linux_x86.sh
+++ builder/init/release.linux_x86.sh
@@ -14,14 +14,8 @@ setup_gh() {
     return
   fi
 
-#  dirname="gh_${GH_VERSION}_linux_amd64"
-#  filename="${dirname}".tar.gz
   curl -OL https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.deb
   apt install ./gh_${GH_VERSION}_linux_amd64.deb
-#  curl -OL https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.tar.gz
-#  tar -xzvf "${filename}"
-#  cp "$dirname/bin/gh" ~/bin
-#  chmod u+x ~/bin/gh
 }
 
 main
